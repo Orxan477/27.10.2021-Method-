e057ee75507cfe59b3d213c9d00b34377bf7d02b
Ganbarov <tu201906147@aztu.local>
2021-10-27 17:06:39 +0400

    Sinif

---
+++ Method-classTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method_classTask
+{
+    class Program
+    {
+        #region menfileri musbet etmek
+        static void Main(string[] args)
+        {
+            int[] arr = { -1, 36, -50 };
+            massiv(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static int[] massiv(int[] musbet)
+        {
+
+            for (int i = 0; i < musbet.Length; i++)
+            {
+                if (musbet[i] < 0)
+                {
+                    musbet[i] = musbet[i] * (-1);
+                }
+            }
+            return musbet;
+        }
+        #endregion  
+    }
+}
